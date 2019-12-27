f02cd903bc27339c83af36de92e91af3584bfd9e
柴进 <380738762@qq.com>
2019-12-27 11:43:44 +0800

    ✨ add element-ui plugin add .vscode config change .prettierrc

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import '@/assets/css/main.css'
+import './plugins/element.js'
 
 Vue.config.productionTip = false
 
--- src/views/About.vue
+++ src/views/About.vue
@@ -3,3 +3,9 @@
     <h1 class="text-pink-500">This is an about page</h1>
   </div>
 </template>
+
+<script>
+export default {
+  name: 'about'
+}
+</script>
