2824cae893ca80404bb193643e203b0b3fcb5ff7
柴进 <380738762@qq.com>
2020-01-07 17:06:10 +0800

    💄 upgrade helloworld

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -124,16 +124,21 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="postcss">
+li {
+  @apply inline-block;
+}
+
+ul {
+  @apply list-none p-0;
+}
+</style>
+
 <style scoped lang="stylus">
 h3
   margin 40px 0 0
 
-ul
-  list-style-type none
-  padding 0
-
 li
-  display inline-block
   margin 0 10px
 
 a
